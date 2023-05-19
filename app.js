d655d875aec7c5970f598c5601e6229d306e5125
Adolfo Sánchez Lopéz <sanchezlopezadolfo@gmail.com>
2023-05-19 18:08:06 -0500

    Updating the language of the files

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
+//INITIAL VARIABLES
 let answer = null;
 const array = [];
 
-alert('Hola, Vamos a hacer una lista de números');
+alert('Hello, lets make a list of numbers');
 
-answer = Number (prompt('Ingresa un número del 1 al 50'));
+answer = Number (prompt('Enter a number from 1 to 50'));
 
-while (answer<=0 || answer>=51){
-    answer = prompt('El número que ingreso está fuera del rango, por favor ingresa un número del 1 al 50');
+while (answer<=0 || answer>=51) {
+    answer = prompt('The number you entered is out of range, please enter a number from 1 to 50');
 }
 
-
-
-for (let i = 0; i <= 50; i++){
+for (let i = 0; i <= 50; i++) {
     if (answer >= i){
-        array.push(i);
-    }else{
-        i = 51;
+    array.push(i);
+    } else {
+    i = 51;
     }
-    
 }
 
-alert('Contamos tu número con el siguiente arreglo ['+array+'] y tu número es '+ answer);
+alert('We count your number with the following arrangement ['+array+'] and your number is '+ answer);
